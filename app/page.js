7726f77c4661fb8f0fa735dbf98a568a19d2284f
[Suraj] <[mastersurajsharma04@gmail.com]>
2025-09-29 10:48:08 +0530

    sidebar

--- app/page.js
+++ app/page.js
@@ -3,7 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      home page by suraj
+      <div className="sidebar">
+        <h2>Modules</h2>
+        <ul>
+          <li>Home</li>
+          <li>Leads</li>
+          <li>Contacts</li>
+          <li>Accounts</li>
+          <li>Deals</li>
+          <li>Forcasts</li>
+          <li>Documents</li>
+          <li>Campaigns</li>
+          <li>Tasks</li>
+          <li>Meetings</li>
+          <li>Calls</li>
+          <li>Products</li>
+        </ul>
+      </div>
     </div>
   );
 }
